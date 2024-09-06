af47cad4364b0e06990604e5f628e8cd63267238
Prescott Murphy <prurph@gmail.com>
2024-09-06 12:07:23 -0400

    Complete Exercise 08

--- src/02-globals/08-add-to-window.problem.ts
+++ src/02-globals/08-add-to-window.problem.ts
@@ -1,14 +1,11 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-/**
- * Clues:
- *
- * 1. You'll need declare global again
- *
- * 2. Inside declare global, you'll need to modify the Window
- * interface to add a makeGreeting function
- */
+declare global {
+  interface Window {
+    makeGreeting: () => string;
+  }
+}
 
 window.makeGreeting = () => "Hello, world!";
 
