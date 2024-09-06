8b301c4ae0461afc9ebbf5a85045c1d0e16cf0fc
Prescott Murphy <prurph@gmail.com>
2024-09-06 09:57:55 -0400

    Complete Exercise 05

--- src/01-branded-types/05-currency-conversion.problem.ts
+++ src/01-branded-types/05-currency-conversion.problem.ts
@@ -7,25 +7,35 @@ interface User {
   maxConversionAmount: number;
 }
 
+type AuthorizedUser = Brand<User, "AuthorizedUser">;
+type ConvertedAmount = Brand<number, "ConvertedAmount">;
+
 // Mocks a function that uses an API to convert
 // One currency to another
 const getConversionRateFromApi = async (
   amount: number,
   from: string,
-  to: string,
+  to: string
 ) => {
-  return Promise.resolve(amount * 0.82);
+  return Promise.resolve((amount * 0.82) as ConvertedAmount);
 };
 
 // Mocks a function which actually performs the conversion
-const performConversion = async (user: User, to: string, amount: number) => {};
+const performConversion = async (
+  user: AuthorizedUser,
+  to: string,
+  amount: ConvertedAmount
+) => {};
 
-const ensureUserCanConvert = (user: User, amount: number): User => {
+const ensureUserCanConvert = (
+  user: User,
+  amount: ConvertedAmount
+): AuthorizedUser => {
   if (user.maxConversionAmount < amount) {
     throw new Error("User cannot convert currency");
   }
 
-  return user;
+  return user as AuthorizedUser;
 };
 
 describe("Possible implementations", () => {
@@ -34,7 +44,7 @@ describe("Possible implementations", () => {
       user: User,
       from: string,
       to: string,
-      amount: number,
+      amount: number
     ) => {
       const convertedAmount = await getConversionRateFromApi(amount, from, to);
 
@@ -48,7 +58,7 @@ describe("Possible implementations", () => {
       user: User,
       from: string,
       to: string,
-      amount: number,
+      amount: number
     ) => {
       // @ts-expect-error
       const authorizedUser = ensureUserCanConvert(user, amount);
@@ -63,7 +73,7 @@ describe("Possible implementations", () => {
       user: User,
       from: string,
       to: string,
-      amount: number,
+      amount: number
     ) => {
       const convertedAmount = await getConversionRateFromApi(amount, from, to);
       const authorizedUser = ensureUserCanConvert(user, convertedAmount);
