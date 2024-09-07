045b14ca5a34722ed7ac80f69a34a812ab872499
Prescott Murphy <prurph@gmail.com>
2024-09-07 09:07:38 -0400

    Complete Exercise 18

--- src/04-classes/18-type-predicates-and-classes.problem.ts
+++ src/04-classes/18-type-predicates-and-classes.problem.ts
@@ -5,10 +5,10 @@ class Form<TValues> {
 
   constructor(
     public values: TValues,
-    private validate: (values: TValues) => string | void,
+    private validate: (values: TValues) => string | void
   ) {}
 
-  isInvalid() {
+  isInvalid(): this is this & { error: string } {
     const result = this.validate(this.values);
 
     if (typeof result === "string") {
@@ -34,7 +34,7 @@ const form = new Form(
     if (!values.password) {
       return "Password is required";
     }
-  },
+  }
 );
 
 if (form.isInvalid()) {
