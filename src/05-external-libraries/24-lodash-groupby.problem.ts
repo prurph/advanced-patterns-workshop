473ad1e4e996013905de6beb82285fb3fe5f2983
Prescott Murphy <prurph@gmail.com>
2024-09-07 15:26:31 -0400

    Complete Exercise 24

--- src/05-external-libraries/24-lodash-groupby.problem.ts
+++ src/05-external-libraries/24-lodash-groupby.problem.ts
@@ -2,15 +2,7 @@ import _ from "lodash";
 import { expect, it } from "vitest";
 import { doNotExecute, Equal, Expect } from "../helpers/type-utils";
 
-/**
- * We've made a reusable function here to group
- * arrays of objects by age. I want you to:
- *
- * 1. Make sure that the errors (below) disappear
- * 2. Take a look at the typings for _.groupBy to
- * see if you can understand them.
- */
-const groupByAge = (array: unknown[]) => {
+const groupByAge = <T extends { age: number }>(array: T[]) => {
   const grouped = _.groupBy(array, "age");
 
   return grouped;
