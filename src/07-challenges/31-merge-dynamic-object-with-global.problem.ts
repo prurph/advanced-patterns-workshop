dedc63376944585cebe9ad43af8bba9d9ca26900
Prescott Murphy <prurph@gmail.com>
2024-09-09 08:58:18 -0400

    Complete Exercise 31

--- src/07-challenges/31-merge-dynamic-object-with-global.problem.ts
+++ src/07-challenges/31-merge-dynamic-object-with-global.problem.ts
@@ -9,9 +9,14 @@ const addAllOfThisToWindow = {
 
 Object.assign(window, addAllOfThisToWindow);
 
+declare global {
+  type TAddAllOfThisToWindow = typeof addAllOfThisToWindow;
+  interface Window extends TAddAllOfThisToWindow {}
+}
+
 type tests = [
   Expect<Equal<typeof window.add, (a: number, b: number) => number>>,
   Expect<Equal<typeof window.subtract, (a: number, b: number) => number>>,
   Expect<Equal<typeof window.multiply, (a: number, b: number) => number>>,
-  Expect<Equal<typeof window.divide, (a: number, b: number) => number>>,
+  Expect<Equal<typeof window.divide, (a: number, b: number) => number>>
 ];
